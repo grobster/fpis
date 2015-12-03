1f576c66077d8f21dd00f68d29aeb19e030564a2
grobster <jquarles@grobster.com>
2015-12-03 10:04:26 -0500

    added dropWhile function.  Exercise 3.5

--- listings/3.1/List.scala
+++ listings/3.1/List.scala
@@ -36,4 +36,9 @@ object List {
 		_drop(l, n, Nil)
 	}
 	
+	def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+		case Cons(h, t) if(f(h)) => dropWhile(t, f)
+		case _ => l
+	}
+	
 }

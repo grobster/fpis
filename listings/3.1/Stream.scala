f44576eab7f256260508ae333d5a38eefc5ddfdc
grobster <jquarles@grobster.com>
2015-12-08 16:06:27 -0500

    added toList function

--- listings/3.1/Stream.scala
+++ listings/3.1/Stream.scala
@@ -1,6 +1,15 @@
 package com.grobster.fpinscala.stream
 
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+	def toList: List[A] = {
+		def go(s: Stream[A], acc: List[A]): List[A] = s match {
+			case Cons(h,t) => go(t(), h() :: acc)
+			case _ => acc
+		}
+		go(this, List()).reverse
+	}
+}
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 

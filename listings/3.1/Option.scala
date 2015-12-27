50a7a635dd28a846cb4687bf8824fb6705bb0c32
grobster <jeff.quarles@gmail.com>
2015-12-27 06:19:44 -0500

    implemented methods in Option trait

--- listings/3.1/Option.scala
+++ listings/3.1/Option.scala
@@ -1,9 +1,31 @@
 package com.grobster.fpinscala.stream
 
 trait Option[+A] {
-	def map[B](f: A => B): Option[B]
-	def flatMap[B](f: A => Option[B]): Option[B]
-	def getOrElse[B >: A](default: => B): B
-	def orElse[B >: A](obj: => Option[B]): Option[B]
-	def filter(f: A => Boolean): Option[A]
+	def map[B](f: A => B): Option[B] = this match {
+		case None => None
+		case Some(a) => Some(f(a))
+	}
+	
+	def flatMap[B](f: A => Option[B]): Option[B] = this match {
+		case None => None
+		case Some(a) => f(a)
+	}
+	
+	def getOrElse[B >: A](default: => B): B = this match {
+		case None => default
+		case Some(a) => a
+	}
+	
+	def orElse[B >: A](obj: => Option[B]): Option[B] = this match {
+		case None => obj
+		case _ => this
+	}
+	
+	def filter(f: A => Boolean): Option[A] = this match {
+		case Some(a) if(f(a)) => this
+		case _ => None
+	}
 }
+
+case class Some[+A](value: A) extends Option[A]
+case object None extends Option[Nothing]

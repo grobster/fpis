bc18b1a14dc0d888d86d74c444383a75110701f7
grobster <jquarles@grobster.com>
2016-01-24 07:58:18 -0500

    added mean function

--- listings/3.1/Option.scala
+++ listings/3.1/Option.scala
@@ -29,3 +29,7 @@ trait Option[+A] {
 
 case class Some[+A](value: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Option {
+	def mean(xs: Seq[Double]): Option[Double] = if(xs.isEmpty) None else Some(xs.sum / xs.length)
+}

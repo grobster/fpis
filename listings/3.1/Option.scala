ba36a636d0a148f8a4139bb5679c082a22c84380
grobster <jquarles@grobster.com>
2016-01-24 08:08:33 -0500

    added variance function

--- listings/3.1/Option.scala
+++ listings/3.1/Option.scala
@@ -32,4 +32,6 @@ case object None extends Option[Nothing]
 
 object Option {
 	def mean(xs: Seq[Double]): Option[Double] = if(xs.isEmpty) None else Some(xs.sum / xs.length)
+	
+	def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 }

579d54bf1d87fe1632e62e4111b4edbf58bb64fb
grobster <jquarles@grobster.com>
2016-02-07 16:09:56 -0500

    added unfold to Stream object

--- listings/3.1/Stream.scala
+++ listings/3.1/Stream.scala
@@ -56,11 +56,6 @@ sealed trait Stream[+A] {
 	def constant[A](a: A): Stream[A] =  { lazy val tail: Stream[A] = Cons(() => a, () => tail); tail }
 	
 	def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
-	
-	def unfold[A,S](z: S)(f: S => Option[(A,S)]): Stream[A] = f(z) match {
-		case Some((h,s)) => Stream.cons(h, unfold(s)(f))
-		case None => Stream.empty
-	}
 }
 
 case object Empty extends Stream[Nothing]
@@ -78,6 +73,11 @@ object Stream{
 	val fibs = {
 		def go(f0: Int, f1: Int): Stream[Int] = Stream.cons(f0, go(f1, f0 + f1))
 		go(0, 1)
+	}
+	
+	def unfold[A,S](z: S)(f: S => Option[(A,S)]): Stream[A] = f(z) match {
+		case Some((h,s)) => cons(h, unfold(s)(f))
+		case None => empty
 	}
 	
 	def apply[A](as: A*): Stream[A] = if(as.isEmpty) empty else cons(as.head, apply(as.tail: _*))

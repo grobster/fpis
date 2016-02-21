1884cdf42fc16ed11fbb3583cbe51b97b166da5b
grobster <jeff.quarles@gmail.com>
2016-02-21 06:27:41 -0500

    added map function

--- ch 6/RNG.scala
+++ ch 6/RNG.scala
@@ -51,4 +51,9 @@ case class SimpleRNG(seed: Long) extends RNG {
 	type Rand[+A] = RNG => (A, RNG)
 	
 	def unit[A](a: A): Rand[A] = rng => (a,rng)
+	
+	def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+		val (a, rng2) = s(rng)
+		(f(a), rng2)
+	}
 }

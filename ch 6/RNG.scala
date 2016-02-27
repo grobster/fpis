d18f28fc13e485116ab3ec0492bef850808cedac
grobster <jeff.quarles@gmail.com>
2016-02-26 21:27:48 -0500

    added case class to object

--- ch 6/RNG.scala
+++ ch 6/RNG.scala
@@ -2,16 +2,17 @@ trait RNG {
 	def nextInt: (Int, RNG)
 }
 
-case class SimpleRNG(seed: Long) extends RNG {
-	def nextInt: (Int, RNG) = {
-		val newSeed = (seed * 0x5DEECE66DL) + 0xFFFFFFFFFFFFL
-		val nextRNG = SimpleRNG(newSeed)
-		val n = (newSeed >>> 16).toInt
-		(n, nextRNG)
-	}
-}
+object RNG {
 
-case object RNG {
+
+	case class SimpleRNG(seed: Long) extends RNG {
+		def nextInt: (Int, RNG) = {
+			val newSeed = (seed * 0x5DEECE66DL) + 0xFFFFFFFFFFFFL
+			val nextRNG = SimpleRNG(newSeed)
+			val n = (newSeed >>> 16).toInt
+			(n, nextRNG)
+		}
+	}
 	
 	def nonNegativeInt(rng: RNG): (Int, RNG) = {
 		val (i,r) = rng.nextInt

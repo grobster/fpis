0a6469d2c6e88df1d1e1a1575c37454e514bd0fc
grobster <jeff.quarles@gmail.com>
2016-03-05 00:13:05 -0500

    removed SimpleRNG class from object

--- ch 6/RNG.scala
+++ ch 6/RNG.scala
@@ -2,18 +2,16 @@ trait RNG {
 	def nextInt: (Int, RNG)
 }
 
-object RNG {
-
-
-	case class SimpleRNG(seed: Long) extends RNG {
-		def nextInt: (Int, RNG) = {
-			val newSeed = (seed * 0x5DEECE66DL) + 0xFFFFFFFFFFFFL
-			val nextRNG = SimpleRNG(newSeed)
-			val n = (newSeed >>> 16).toInt
-			(n, nextRNG)
-		}
+case class SimpleRNG(seed: Long) extends RNG {
+	def nextInt: (Int, RNG) = {
+		val newSeed = (seed * 0x5DEECE66DL) + 0xFFFFFFFFFFFFL
+		val nextRNG = SimpleRNG(newSeed)
+		val n = (newSeed >>> 16).toInt
+		(n, nextRNG)
 	}
-	
+}
+
+object RNG {	
 	def nonNegativeInt(rng: RNG): (Int, RNG) = {
 		val (i,r) = rng.nextInt
 		(if(i < 0) -(i + 1) else i, r)
